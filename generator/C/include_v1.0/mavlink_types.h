93e6fb7739c357aa7fc05560f09e67677d0afd66
LM <lm@student.ethz.ch>
2011-11-04 15:02:57 +0100

    Minor non-const fixes to comm decoding

--- generator/C/include_v1.0/mavlink_types.h
+++ generator/C/include_v1.0/mavlink_types.h
@@ -127,11 +127,11 @@ typedef struct __mavlink_message_info {
 } mavlink_message_info_t;
 
 #define _MAV_PAYLOAD(msg) ((const char *)(&(msg)->payload64[0]))
-#define _MAV_PAYLOAD_NON_CONST(msg) (char *)((char *)(&(msg)->payload64[0]))
+#define _MAV_PAYLOAD_NON_CONST(msg) ((char *)((char *)(&(msg)->payload64[0])))
 
 // checksum is immediately after the payload bytes
-#define mavlink_ck_a(msg) *(msg->len + (uint8_t *)_MAV_PAYLOAD(msg))
-#define mavlink_ck_b(msg) *((msg->len+(uint16_t)1) + (uint8_t *)_MAV_PAYLOAD(msg))
+#define mavlink_ck_a(msg) *(msg->len + (uint8_t *)_MAV_PAYLOAD_NON_CONST(msg))
+#define mavlink_ck_b(msg) *((msg->len+(uint16_t)1) + (uint8_t *)_MAV_PAYLOAD_NON_CONST(msg))
 
 typedef enum {
     MAVLINK_COMM_0,
