49f452f18c83e1148578ccac2313dad12f389bda
Andrew Radford <andrew@radford.net.nz>
2011-11-06 17:05:17 +0000

    Adding Monitor console

---
+++ generator/CS/Mavlink_Monitor_Console/ConsoleDumper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MavlinkStructs;
+
+namespace Mavlink_Monitor_Console
+{
+    /// <summary>
+    /// Dumps all stuff from mavlink to the console
+    /// </summary>
+    public class ConsoleDumper
+    {
+        private Mavlink_Network _mavNet;
+
+        private static string lineFormat = "Sys:{0} Comp:{1} Msg:{2}";
+
+        public ConsoleDumper(Mavlink_Network mavNet)
+        {
+            _mavNet = mavNet;
+            _mavNet.PacketReceived += new PacketReceivedEventHandler(networkLayer_PacketReceived);
+        }
+
+        void networkLayer_PacketReceived(object sender, MavlinkPacket e)
+        {
+            Console.WriteLine(lineFormat, e.SystemId, e.ComponentId, e.Message.GetType().Name);
+        }
+    }
+}
---
+++ generator/CS/Mavlink_Monitor_Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MavlinkStructs;
+using System.IO;
+
+namespace Mavlink_Monitor_Console
+{
+    /// <summary>
+    /// Program to print out to console items received on a mavlink link
+    /// or from a log file
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var link = new Mavlink_Link();
+            var net = new Mavlink_Network(link, new MavlinkFactory());
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage bla bla");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            var fileName = args[0];
+            var bytes = File.ReadAllBytes(fileName);
+
+            var consoledumper = new ConsoleDumper(net);
+
+            link.AddReadBytes(bytes);
+
+            Console.ReadKey();
+        }
+    }
+}
