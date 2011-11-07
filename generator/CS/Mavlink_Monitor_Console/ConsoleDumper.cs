b1c403b32eccae0cc4414b3870c93715bbe29f08
Andrew Radford <andrew@radford.net.nz>
2011-11-07 20:00:56 +0000

    Console Dumper work

--- generator/CS/Mavlink_Monitor_Console/ConsoleDumper.cs
+++ generator/CS/Mavlink_Monitor_Console/ConsoleDumper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MavlinkStructs;
+using MavLink;
 
 namespace Mavlink_Monitor_Console
 {
@@ -13,17 +14,27 @@ namespace Mavlink_Monitor_Console
     {
         private Mavlink_Network _mavNet;
 
-        private static string lineFormat = "Sys:{0} Comp:{1} Msg:{2}";
+        private static string lineFormat = "Sys: {0} Comp: {1} Msg:{2}";
 
         public ConsoleDumper(Mavlink_Network mavNet)
         {
             _mavNet = mavNet;
             _mavNet.PacketReceived += new PacketReceivedEventHandler(networkLayer_PacketReceived);
-        }
+
+                 }
 
         void networkLayer_PacketReceived(object sender, MavlinkPacket e)
         {
-            Console.WriteLine(lineFormat, e.SystemId, e.ComponentId, e.Message.GetType().Name);
+            if (e.Message is MAVLink_vfr_hud_message)
+                ShowMessage((MAVLink_vfr_hud_message)e.Message);
+            else
+                Console.WriteLine(lineFormat, e.SystemId, e.ComponentId, e.Message.GetType().Name);
+        }
+
+        private void ShowMessage(MAVLink_vfr_hud_message msg)
+        {
+            string lineFormat = "Alt: {0} Heading: {1} Throttle: {2}";
+            Console.WriteLine(lineFormat,  msg.alt, msg.heading, msg.throttle);
         }
     }
 }
