997e94a18f2168d9c58c7ab2c9720e4a492465dc
Andrew Radford <andrew@radford.net.nz>
2011-11-08 19:17:39 +0000

    Fix to gen script

--- generator/gen_all.sh
+++ generator/gen_all.sh
@@ -4,7 +4,7 @@ for protocol in 0.9 1.0; do
  for xml in message_definitions/v$protocol/*.xml; do
      base=$(basename $xml .xml)
      ./mavgen.py --lang=C --wire-protocol=$protocol --output=C/include_v$protocol $xml || exit 1
-     ./mavgen.py --lang=CS --wire-protocol=%protocol --output=CS/${base}_v$protocol/mavlink_messages $xml || exit 1
+     ./mavgen.py --lang=CS --wire-protocol=$protocol --output=CS/${base}_v$protocol/mavlink_messages $xml || exit 1
      ./mavgen.py --lang=python --wire-protocol=$protocol --output=python/mavlink_${base}_v$protocol.py $xml || exit 1
  done
 done
