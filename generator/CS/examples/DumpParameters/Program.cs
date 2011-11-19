813cd378d69b6ed6eda74f396deb6e1f0200590e
Andrew Radford <andrew@radford.net.nz>
2011-11-19 15:33:35 +0000

    Dump Params example program now working

--- generator/CS/examples/DumpParameters/Program.cs
+++ generator/CS/examples/DumpParameters/Program.cs
@@ -9,51 +9,58 @@ namespace DumpParameters
 {
     /*
      * Simple program to listen for Mavlink systems, and when
-     * they are found, dump the parameter list etc
+     * they are found, request and dump the parameter list 
      * 
      * If the stream is a file, not a serial port, then a request
      * will not be made, but any param packets encountered will be
      * dumped
+     * 
+     * -F "../../2011-09-22 07-00-05.tlog"
+     * 
      */
     class Program
     {
         private static Stream _mavStream;
         private static MavlinkNetwork _net;
+        private static Mavlink_Link _link;
 
         static void Main(string[] args)
         {
             _mavStream = ExampleCommon.GetMavStreamFromArgs(args);
-            var link = new Mavlink_Link(_mavStream);
-            _net = new MavlinkNetwork(link);
-
+            
+            
+            _link = new Mavlink_Link(_mavStream);
+            _net = new MavlinkNetwork(_link);
+            
             var packetsReceived = Observable.FromEvent<PacketReceivedEventHandler, MavlinkPacket>(
                 handler => (sender, e) => handler.Invoke(e),
                 d => _net.PacketReceived += d,
                 d => _net.PacketReceived -= d);
             
             var heartbeats = packetsReceived.Where(p => p.Message is MAVLink_heartbeat_message);
-
+            
             Console.WriteLine("Waiting For hearbeat (10 second timeout)...");
 
-            var paramsReceived = from hb in heartbeats.Take(1).Do(SendParamListRequest).Timeout(TimeSpan.FromSeconds(10))
+            var paramsReceived = from hb in heartbeats
+                                     .Take(1)
+                                     .Do(SendParamListRequest)
+                                     .Timeout(TimeSpan.FromSeconds(10))
                       from message in packetsReceived
                           .Where(m => m.SystemId == hb.SystemId)
                           .Where(m => m.ComponentId == hb.ComponentId)
                           .Select(p => p.Message)
                           .OfType<MAVLink_param_value_message>()
+                          .TakeWhile(p => p.param_index < p.param_count - 1 )
                       select message;
 
-            var lastParamReceived = paramsReceived.Where(m => m.param_index == m.param_count - 1);
+            paramsReceived.Subscribe(
+                        DumpPacket, 
+                        e => Console.WriteLine("Error: " + e.Message),
+                        () => Console.WriteLine("Complete"));
 
-            paramsReceived
-                .TakeUntil(lastParamReceived)
-                .Subscribe(DumpPacket, 
-                        e => { Console.WriteLine("Error: " + e.Message); },
-                        () => { Console.WriteLine("Complete"); });
-
-            link.Start();
+            _link.Start();
             Console.ReadKey();
-            link.Stop();
+            _link.Stop();
             _mavStream.Close();
         }
 
@@ -76,7 +83,8 @@ namespace DumpParameters
                     target_component = (byte) mavlinkPacket.ComponentId,
                 };
 
-                _net.Send(new MavlinkPacket { ComponentId = 1, SystemId = 255, Message = req });
+                var bytes = _net.Send(new MavlinkPacket { ComponentId = 1, SystemId = 255, Message = req });
+                _link.SendPacket(bytes);
             }
             else
             {
