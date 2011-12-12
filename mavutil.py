f7a8815f02f7e5d22739a3191760810fe6acbccf
Andrew Tridgell <tridge@samba.org>
2011-12-12 22:38:44 +1100

    cope with : in log file names

--- mavutil.py
+++ mavutil.py
@@ -451,7 +451,7 @@ def mavlink_connection(device, baud=115200, source_system=255,
         return mavtcp(device[4:], source_system=source_system)
     if device.startswith('udp:'):
         return mavudp(device[4:], input=input, source_system=source_system)
-    if device.find(':') != -1:
+    if device.find(':') != -1 and not device.endswith('log'):
         return mavudp(device, source_system=source_system, input=input)
     if os.path.isfile(device):
         if device.endswith(".elf"):
